8d7e00377aa9d6fa514db627d1826b5a2f286a7e
cesarandres17 <cesara.ramirezr@uqvirtual.edu.co>
2024-11-17 08:38:52 -0500

    CAMBIOS

--- proyectofinal/src/main/java/co/edu/uniquindio/poo/Compra.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public class Compra extends Transaccion {
-    // Constructor que llama al constructor de la clase base (Transaccion)
-    public Compra(LocalDate fecha, Empleado empleado, Cliente cliente, Vehiculo vehiculo) {
-        super(fecha, empleado, cliente, vehiculo);
-    }
-}
--- proyectofinal/src/main/java/co/edu/uniquindio/poo/Vehiculo.java
+++ proyectofinal/src/main/java/co/edu/uniquindio/poo/Vehiculo.java
@@ -6,6 +6,7 @@ public abstract class Vehiculo {
     private boolean nuevo;
     private String transmision;
     private String tipoCombustible;
+    private Combustible combustible;
 
     // Constructor
     public Vehiculo(String marca, int modelo, boolean nuevo, String transmision, String tipoCombustible) {
@@ -56,4 +57,22 @@ public abstract class Vehiculo {
     public void setTipoCombustible(String tipoCombustible) {
         this.tipoCombustible = tipoCombustible;
     }
+
+    public Combustible getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(Combustible combustible) {
+        this.combustible = combustible;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo [marca=" + marca + ", modelo=" + modelo + ", nuevo=" + nuevo + ", transmision=" + transmision
+                + ", tipoCombustible=" + tipoCombustible + ", combustible=" + combustible + "]";
+    }
+    
+
+
+
 }
