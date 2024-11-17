792216c3a2685e6dca630fe1bbc3f70720b21411
cesarandres17 <cesara.ramirezr@uqvirtual.edu.co>
2024-11-17 08:40:25 -0500

    BORRAR

--- proyectofinal/src/main/java/co/edu/uniquindio/poo/Alquiler.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public class Alquiler extends Transaccion {
-    // Constructor que llama al constructor de la clase base (Transaccion)
-    public Alquiler(LocalDate fecha, Empleado empleado, Cliente cliente, Vehiculo vehiculo) {
-        super(fecha, empleado, cliente, vehiculo);
-    }
-}
-
--- proyectofinal/src/main/java/co/edu/uniquindio/poo/Venta.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public class Venta extends Transaccion {
-    // Constructor que llama al constructor de la clase base (Transaccion)
-    public Venta(LocalDate fecha, Empleado empleado, Cliente cliente, Vehiculo vehiculo) {
-        super(fecha, empleado, cliente, vehiculo);
-    }
-}
