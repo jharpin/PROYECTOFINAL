22312e8e7bbddeb3205a45a0f0e81c5472dfb570
jharpin <154296932+jharpin@users.noreply.github.com>
2024-11-17 23:32:43 -0500

    mejoras

--- proyectofinalfx/src/main/java/module-info.java
+++ proyectofinalfx/src/main/java/module-info.java
@@ -4,13 +4,13 @@ module co.edu.uniquindio.proyecto_final.tucarro {
     requires java.desktop;
     requires itextpdf;
     requires org.jfree.jfreechart;
-    requires  org.junit.jupiter.engine;
+    requires org.junit.jupiter.engine;
     requires org.junit.jupiter.api;
     requires org.testng;
 
 
-    opens co.edu.uniquindio.proyecto_final.tucarro to javafx.fxml;
-    opens co.edu.uniquindio.proyecto_final.tucarro.viewcontroller to javafx.fxml;
-    exports co.edu.uniquindio.proyecto_final.tucarro;
+    opens co.edu.uniquindio.poo.tucarro to javafx.fxml;
+    opens co.edu.uniquindio.poo.viewcontroller to javafx.fxml;
+    exports co.edu.uniquindio.poo;
 
 }
